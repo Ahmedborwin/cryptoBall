f7740a651a508add644f857f922f085b76d4e1e1
Ampck <a1j2m4@gmail.com>
2024-05-05 10:22:58 -0400

    cleaned up gameEngine.js

--- gameEngine.js
+++ gameEngine.js
@@ -38,33 +38,7 @@ function simulateMatch() {
   team2Goals = team2Goals - ((team2Goals) * (team1GkSkill * eliteGoalSavePercentage))
 
   const winner = (team1Goals < team2Goals) ? 0 : 1;
-
-  /*
-
-  let scoreTeam1 =
-    team1.reduce((acc, player) => {
-      return acc + player[2] + player[3] + player[4] // Assuming Attack, Midfield, Defense are at indexes 2, 3, 4
-    }, 0) /
-      1000 +
-    (randomFactors[0] % 3)
-
-  let scoreTeam2 =
-    team2.reduce((acc, player) => {
-      console.log("Processing player for Team 2:", player[1]) // Assuming player name is at index 1
-      return acc + player[2] + player[3] + player[4] // Same assumption for indexes
-    }, 0) /
-      1000 +
-    (randomFactors[1] % 3)
-
-  const goalsTeam1 = Math.round(scoreTeam1)
-  const goalsTeam2 = Math.round(scoreTeam2)
-  console.log("Goals calculated for both teams.", goalsTeam1, goalsTeam2)
-
-  adjustPlayerAttributes(team1, goalsTeam1 >= goalsTeam2 ? randomFactors[2] : -randomFactors[3])
-  adjustPlayerAttributes(team2, goalsTeam2 >= goalsTeam1 ? randomFactors[3] : -randomFactors[2])
-  console.log("Player attributes adjusted based on match results.")
-  */
-  return { winner }
+  return { winner, team1Goals, team2Goals }
 }
 
 function computeScoreFromChance(skill, chance) {
@@ -112,15 +86,6 @@ function encodeMatchResult(matchResult) {
   const goalsTeam1Bytes = Buffer.from([matchResult.goalsTeam1]) // Simple example
   const goalsTeam2Bytes = Buffer.from([matchResult.goalsTeam2])
 
-  console.log(
-    "Team 1 data for Buffer creation:",
-    matchResult.team1.map((player) => player)
-  )
-  console.log(
-    "Team 2 data for Buffer creation:",
-    matchResult.team2.map((player) => player)
-  )
-
   const team1Bytes = Buffer.concat(
     matchResult.team1.map((player) => {
       return Buffer.from(new Uint8Array(player))
@@ -132,33 +97,14 @@ function encodeMatchResult(matchResult) {
     })
   )
 
+
+
   // Concatenate all bytes
   const encodedBytes = Buffer.concat([goalsTeam1Bytes, goalsTeam2Bytes, team1Bytes, team2Bytes])
 
   return encodedBytes
 }
 
-async function compressBuffer(buffer) {
-  try {
-    const compressed = await gzipAsync(buffer)
-
-    console.log(`Original size: ${buffer.length}, Compressed size: ${compressed.length}`)
-    return compressed
-  } catch (error) {
-    console.error("Compression error:", error)
-    return null
-  }
-}
-
 // Simulate and encode the match
 const matchResult = simulateMatch()
 const encodedMatchResult = encodeMatchResult(matchResult)
-
-compressBuffer(encodedMatchResult).then((compressedResult) => {
-  if (compressedResult) {
-    console.log("Compression successful")
-    console.log(`The size of the encodedMatchResult buffer is: ${compressedResult.length} bytes`)
-    console.log(compressedResult)
-    return compressedResult
-  }
-})

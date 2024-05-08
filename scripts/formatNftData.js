4b7ed5bfa134b737c15caa46f896864873f0b5ea
Ampck <a1j2m4@gmail.com>
2024-05-07 20:20:58 -0400

    added MatchManager contract

---
+++ scripts/formatNftData.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+// Read data from the JSON file
+const rawData = fs.readFileSync('./nft_data/player-data-essentials-pretty.json');
+const data = JSON.parse(rawData);
+
+// Convert data to an object keyed by full names
+const convertedData = data.reduce((acc, player) => {
+    const { full_name, ...attributes } = player;
+    acc[full_name] = attributes;
+    return acc;
+}, {});
+
+// Format the data as a pretty-printed JSON string
+const outputData = JSON.stringify(convertedData, null, 4);
+
+// Write the output to a file
+fs.writeFileSync('output.json', outputData);
+
+console.log('Data has been successfully processed and saved to output.json.');
